72ce230b6e7f6431177cd5ab790cb2504880cc08
Carlos Eduardo <cadumouralemes@gmail.com>
2025-10-22 00:34:57 -0300

    chore: updating dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MoviesModule, FavoritesModule, UsersModule],
-  controllers: [],
+  imports: [ConfigModule.forRoot(), MoviesModule, FavoritesModule, UsersModule, AuthModule],
+  controllers: [AuthController],
   providers: [],
 })
 export class AppModule {}
