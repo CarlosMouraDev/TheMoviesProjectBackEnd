6529803bb9e43885c21685b47b3282dc1a302812
Carlos Eduardo <cadumouralemes@gmail.com>
2025-10-21 22:43:16 -0300

    feat: adding users entity with prisma and create user at /users/register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MoviesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), MoviesModule, FavoritesModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { HashingService } from 'src/common/hashing/hashing.service';
+import { PrismaClient } from 'generated/prisma';
+
+const prisma = new PrismaClient();
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly hashingService: HashingService) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const hashed = await this.hashingService.hash(createUserDto.password);
+    return prisma.user.create({
+      data: { ...createUserDto, password: hashed },
+    });
+  }
+}
